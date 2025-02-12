fd57fbbe023cf5bd32157ca61a2736bfe110aaee
mzerhusen <zerhusma@mail.uc.edu>
2025-02-12 18:50:44 -0500

    Added Rectangle Info

---
+++ src/RectangleInfo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class RectangleInfo {
+    public static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);
+        double rectangleHeight = 0.0;
+        double rectangleWidth = 0.0;
+
+    }
+}
